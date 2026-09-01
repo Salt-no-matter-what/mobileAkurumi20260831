50627766e4d0c43c2ec652568a818d3138c21a80
Kurumi Hashizume <184522882+Salt-no-matter-what@users.noreply.github.com>
2026-09-01 12:16:46 +0900

    TextViewとImageViewの二日目午前の演習

--- app/src/main/java/jp/ac/meijou/android/s241205141/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205141/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ac.meijou.android.s241205141;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205141.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate((getLayoutInflater()));
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding.kurumi.setText(R.string.app_name);
+        binding.imageView.setImageResource(R.drawable.outline_adb_24);
     }
 }
